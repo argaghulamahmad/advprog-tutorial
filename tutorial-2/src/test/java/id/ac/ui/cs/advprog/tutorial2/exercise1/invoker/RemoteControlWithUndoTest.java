28be88501a66f4c4d5bfa7dedfb093a3870a5d7a
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2018-02-21 22:26:29 +0700

    Solve conflict

--- tutorial-2/src/test/java/id/ac/ui/cs/advprog/tutorial2/exercise1/invoker/RemoteControlWithUndoTest.java
+++ tutorial-2/src/test/java/id/ac/ui/cs/advprog/tutorial2/exercise1/invoker/RemoteControlWithUndoTest.java
@@ -42,7 +42,6 @@ class RemoteControlWithUndoTest {
     @Test
     @DisplayName("tells device to undo previous operation when undo button was pressed")
     void testPressUndoButton() {
-        remote.onButtonWasPushed(DEFAULT_BUTTON);
         remote.undoButtonWasPushed();
 
         verify(command).undo();
