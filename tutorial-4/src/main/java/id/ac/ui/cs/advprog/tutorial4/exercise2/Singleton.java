08f54e02997bd244da72852f3c408d0657593024
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2018-03-07 20:57:48 +0700

    Completed mandatory 'exercise2' tutorial4

--- tutorial-4/src/main/java/id/ac/ui/cs/advprog/tutorial4/exercise2/Singleton.java
+++ tutorial-4/src/main/java/id/ac/ui/cs/advprog/tutorial4/exercise2/Singleton.java
@@ -1,12 +1,12 @@
 package id.ac.ui.cs.advprog.tutorial4.exercise2;
 
 public class Singleton {
+    private static Singleton uniqueInstance = new Singleton();
 
-    // TODO Implement me!
-    // What's missing in this Singleton declaration?
+    private Singleton() {
+    }
 
     public static Singleton getInstance() {
-        // TODO Implement me!
-        return null;
+        return uniqueInstance;
     }
 }
