1b051bfc005aa588e3b54c82c6b37540bee055ee
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2018-03-08 16:45:03 +0700

    Completed 69% lines coverage 'exercise1' unittest

--- tutorial-4/src/test/java/id/ac/ui/cs/advprog/tutorial4/exercise1/NewYorkPizzaStoreTest.java
+++ tutorial-4/src/test/java/id/ac/ui/cs/advprog/tutorial4/exercise1/NewYorkPizzaStoreTest.java
@@ -1,4 +1,36 @@
 package id.ac.ui.cs.advprog.tutorial4.exercise1;
 
+import id.ac.ui.cs.advprog.tutorial4.exercise1.pizza.CheesePizza;
+import id.ac.ui.cs.advprog.tutorial4.exercise1.pizza.ClamPizza;
+import id.ac.ui.cs.advprog.tutorial4.exercise1.pizza.Pizza;
+import id.ac.ui.cs.advprog.tutorial4.exercise1.pizza.VeggiePizza;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 public class NewYorkPizzaStoreTest {
+    private PizzaStore store;
+    private Pizza pizza;
+
+    @Before
+    public void setUp() {
+        store = new NewYorkPizzaStore();
+    }
+
+    @Test
+    public void testNewYorkStoreCreatePizzaMethod() {
+        pizza = store.createPizza("veggie");
+        assertNotNull(pizza);
+        assertTrue(pizza instanceof VeggiePizza);
+
+        pizza = store.createPizza("clam");
+        assertNotNull(pizza);
+        assertTrue(pizza instanceof ClamPizza);
+
+        pizza = store.createPizza("cheese");
+        assertNotNull(pizza);
+        assertTrue(pizza instanceof CheesePizza);
+    }
 }
