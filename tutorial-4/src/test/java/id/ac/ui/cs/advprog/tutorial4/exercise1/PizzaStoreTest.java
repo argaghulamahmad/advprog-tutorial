0d848f42ee7d75d0d3df54fa5884b2a947f42412
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2018-03-09 11:11:19 +0700

    Completed 100% lines coverage 'exercise1' unittest

--- tutorial-4/src/test/java/id/ac/ui/cs/advprog/tutorial4/exercise1/PizzaStoreTest.java
+++ tutorial-4/src/test/java/id/ac/ui/cs/advprog/tutorial4/exercise1/PizzaStoreTest.java
@@ -1,5 +1,26 @@
 package id.ac.ui.cs.advprog.tutorial4.exercise1;
 
-class PizzaStoreTest {
+import id.ac.ui.cs.advprog.tutorial4.exercise1.pizza.Pizza;
+import id.ac.ui.cs.advprog.tutorial4.exercise1.pizza.VeggiePizza;
+import org.junit.Before;
+import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class PizzaStoreTest {
+    private PizzaStore store;
+    private Pizza pizza;
+
+    @Before
+    public void setUp() {
+        store = new DepokPizzaStore();
+    }
+
+    @Test
+    public void testPrepareMethod() {
+        pizza = store.orderPizza("veggie");
+        assertNotNull(pizza);
+        assertTrue(pizza instanceof VeggiePizza);
+    }
 }
--- tutorial-4/src/test/java/id/ac/ui/cs/advprog/tutorial4/exercise1/PizzaTestDriveTest.java
+++ tutorial-4/src/test/java/id/ac/ui/cs/advprog/tutorial4/exercise1/PizzaTestDriveTest.java
@@ -1,5 +1,10 @@
 package id.ac.ui.cs.advprog.tutorial4.exercise1;
 
-class PizzaTestDriveTest {
+import org.junit.Test;
 
+public class PizzaTestDriveTest {
+    @Test
+    public void testMainMethodTestDrive() {
+        PizzaTestDrive.main(new String[0]);
+    }
 }
