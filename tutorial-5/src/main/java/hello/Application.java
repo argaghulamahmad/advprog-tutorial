b1b255790c18815ae1a0d32dd931dbbda06ef7be
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2018-03-14 22:20:40 +0700

    Update 'Application.java' src RequestMap('/')

--- tutorial-5/src/main/java/hello/Application.java
+++ tutorial-5/src/main/java/hello/Application.java
@@ -2,9 +2,21 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class Application {
+    @RequestMapping("/")
+    public String welcome() {
+        return "index";
+    }
+
+    @RequestMapping("/index.html")
+    public String index() {
+        return "index";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
