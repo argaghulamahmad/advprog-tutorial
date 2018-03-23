d9b0be0d02c3cabd76b9eedb5bb16b1636a9cff4
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2018-03-23 18:27:01 +0700

    Update test Update test 'tutorial-6/src/test/java/MovieTest.java'.

--- tutorial-6/src/test/java/MovieTest.java
+++ tutorial-6/src/test/java/MovieTest.java
@@ -1,15 +1,20 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
+import java.util.Objects;
 
 public class MovieTest {
 
     private Movie movie;
+    private Movie movie1;
 
     @Before
     public void setUp() {
         movie = new Movie("Who Killed Captain Alex?", Movie.REGULAR);
+        movie1 = new Movie("Ant Man", Movie.REGULAR);
     }
 
     @Test
@@ -34,5 +39,18 @@ public class MovieTest {
         movie.setPriceCode(Movie.CHILDREN);
 
         assertEquals(Movie.CHILDREN, movie.getPriceCode());
+    }
+
+    @Test
+    public void testHashCode() {
+        assertEquals(movie.hashCode(), Objects.hash("Who Killed Captain Alex?", Movie.REGULAR));
+    }
+
+    @Test
+    public void testEquals() {
+        assertFalse(movie.equals(null));
+        assertTrue(movie1.equals(movie1));
+        assertFalse(movie.equals(movie1));
+        assertTrue(movie.equals(new Movie("Who Killed Captain Alex?", Movie.REGULAR)));
     }
 }
