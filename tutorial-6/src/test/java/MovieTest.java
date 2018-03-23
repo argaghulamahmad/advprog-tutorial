ec9277e08844c52ba445eb8fd559ed2a0f70bea1
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2018-03-23 18:29:31 +0700

    Fix codestyle issue

--- tutorial-6/src/test/java/MovieTest.java
+++ tutorial-6/src/test/java/MovieTest.java
@@ -2,9 +2,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Objects;
 import org.junit.Before;
 import org.junit.Test;
-import java.util.Objects;
 
 public class MovieTest {
 
