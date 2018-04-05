33973f2740819f3ee85d8a137a3d04a4ccf4155b
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2018-04-05 19:44:44 +0700

    Add src 'CreditEvaluatorTest.java'

---
+++ tutorial-7/src/test/java/applicant/CreditEvaluatorTest.java
@@ -0,0 +1,28 @@
+package applicant;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CreditEvaluatorTest {
+    private Applicant applicant;
+    private Applicant anotherApplicant;
+    private CreditEvaluator creditEvaluator;
+
+    @Before
+    public void setUp() {
+        applicant = new Applicant();
+        creditEvaluator = new CreditEvaluator(new EmploymentEvaluator(new QualifiedEvaluator()));
+
+        anotherApplicant = mock(Applicant.class);
+        when(anotherApplicant.isCredible()).thenReturn(false);
+    }
+
+    @Test
+    public void testCreditEvaluatorEvaluateMethod() {
+        assertFalse(creditEvaluator.evaluate(anotherApplicant));
+    }
+}
