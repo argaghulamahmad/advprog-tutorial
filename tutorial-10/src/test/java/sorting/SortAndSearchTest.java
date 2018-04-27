ee6d132cdbdd300c11d0c178ffaae8ad3bca8b22
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2018-04-27 22:27:54 +0700

    Make sure there are no code style issues

--- tutorial-10/src/test/java/sorting/SortAndSearchTest.java
+++ tutorial-10/src/test/java/sorting/SortAndSearchTest.java
@@ -9,7 +9,7 @@ public class SortAndSearchTest {
 
     @Test
     public void testQuickSort() {
-        int[] arr = {2, 1, 3, 4, 7, 5, 6, 9, 8, 11, 10, 12 ,15 ,14 ,13};
+        int[] arr = {2, 1, 3, 4, 7, 5, 6, 9, 8, 11, 10, 12, 15, 14, 13};
         arr = Sorter.quickSort(arr);
         int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         assertArrayEquals(arr, expected);
@@ -17,7 +17,7 @@ public class SortAndSearchTest {
 
     @Test
     public void testSearchFound() {
-        int[] arr = {2, 1, 3, 4, 7, 5, 6, 9, 8, 11, 10, 12 ,15 ,14 ,13};
+        int[] arr = {2, 1, 3, 4, 7, 5, 6, 9, 8, 11, 10, 12, 15, 14, 13};
         assertEquals(Finder.quickSearch(arr, 9), 9);
     }
 
