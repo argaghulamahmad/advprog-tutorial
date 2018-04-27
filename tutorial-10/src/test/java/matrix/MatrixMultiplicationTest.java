3de21aed3d6b0b968d39218ce7b590772052495d
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2018-04-27 21:56:16 +0700

    Provide Unit Test to check code

--- tutorial-10/src/test/java/matrix/MatrixMultiplicationTest.java
+++ tutorial-10/src/test/java/matrix/MatrixMultiplicationTest.java
@@ -1,5 +1,45 @@
 package matrix;
 
+import static org.junit.Assert.assertArrayEquals;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
 public class MatrixMultiplicationTest {
-    //TODO Implement, apply your test cases here
+
+    private double[][] matrix1a;
+    private double[][] matrix1b;
+    private double[][] expected1;
+
+    @Before
+    public void setUp() throws IOException {
+        matrix1a = Main.convertInputFileToMatrix(Main.getPathFileMatrix1(), Main.getNumberOfLine1(),
+                Main.getNumberOfLine1());
+        matrix1b = Main.convertInputFileToMatrix(Main.getPathFileMatrix2(), Main.getNumberOfLine2(),
+                Main.getNumberOfLine1());
+        expected1 = Main.convertInputFileToMatrix(Main.getGenericMatrixPath()
+                + "A/matrixResult.txt", 50, 50);
+    }
+
+    @Test
+    public void basicMultiplication() throws InvalidMatrixSizeForMultiplicationException {
+        assertArrayEquals(MatrixOperation.basicMultiplicationAlgorithm(matrix1a, matrix1b),
+                expected1);
+    }
+
+    @Test
+    public void strassenMultiplication() {
+        assertArrayEquals(MatrixOperation.strassenMatrixMultiForNonSquareMatrix(matrix1a,
+                matrix1b), expected1);
+    }
+
+    @Test
+    public void convertNonSquareMatrix() throws IOException {
+        Main.convertInputFileToMatrix(Main.getGenericMatrixPath() + "B/matrix10rows50columns.txt",
+                50, 10);
+    }
+
 }
--- tutorial-10/src/test/java/sorting/SortAndSearchTest.java
+++ tutorial-10/src/test/java/sorting/SortAndSearchTest.java
@@ -1,5 +1,30 @@
 package sorting;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class SortAndSearchTest {
-    //TODO Implement, apply your test cases here
+
+    @Test
+    public void testQuickSort() {
+        int[] arr = {2, 1, 3, 4, 7, 5, 6, 9, 8, 11, 10, 12 ,15 ,14 ,13};
+        arr = Sorter.quickSort(arr);
+        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        assertArrayEquals(arr, expected);
+    }
+
+    @Test
+    public void testSearchFound() {
+        int[] arr = {2, 1, 3, 4, 7, 5, 6, 9, 8, 11, 10, 12 ,15 ,14 ,13};
+        assertEquals(Finder.quickSearch(arr, 9), 9);
+    }
+
+    @Test
+    public void testSearchNotFound() {
+        int[] arr = {2, 1, 3, 4, 7, 5, 6, 9, 8, 11, 10, 12 ,15 ,14 ,13};
+        assertEquals(Finder.quickSearch(arr, 0), -1);
+    }
+
 }
